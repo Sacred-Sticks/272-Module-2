7437e5694801516e3173bf129f32a787adf1248c
Lucas Ackman <75407333+Sacred-Sticks@users.noreply.github.com>
2022-04-29 01:54:08 -0500

    Added Music Templates

--- 272 - Module 2/Assets/Scripts/Audio/ConstantAudioPlayer.cs
+++ 272 - Module 2/Assets/Scripts/Audio/ConstantAudioPlayer.cs
@@ -2,12 +2,20 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
+[RequireComponent(typeof(AudioSource))]
 public class ConstantAudioPlayer : MonoBehaviour
 {
+    [SerializeField] private AudioClip clip;
+
+    private AudioSource audioSource;
+
     // Start is called before the first frame update
     void Start()
     {
-        
+        audioSource = GetComponent<AudioSource>();
+        audioSource.loop = true;
+        audioSource.clip = clip;
+        audioSource.Play();
     }
 
     // Update is called once per frame

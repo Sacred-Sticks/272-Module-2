d4725c310cbf687095425870d86f896d663efb60
Lucas Ackman <75407333+Sacred-Sticks@users.noreply.github.com>
2022-05-05 00:54:02 -0500

    Removed Timer & Started Adding Tutorial Levels

--- 272 - Module 2/Assets/Scripts/Puzzle Mechanics/Stone Curse/TimeManager.cs
+++ 272 - Module 2/Assets/Scripts/Puzzle Mechanics/Stone Curse/TimeManager.cs
@@ -4,44 +4,13 @@ using UnityEngine;
 
 public class TimeManager : MonoBehaviour
 {
-    [SerializeField] private float waitTime;
     [SerializeField] private GameObject stoneStatue;
     [SerializeField] private Transform player;
     [Space]
     [SerializeField] private int numRespawns;
 
     private bool canStartTimer = true;
-    private float timer;
     private bool canSpawn;
-
-    private void Start()
-    {
-        timer = waitTime;
-        StartCoroutine("StartTimer");
-    }
-
-    // Update is called once per frame
-    void Update()
-    {
-        if (numRespawns > 0) timer -= Time.deltaTime;
-        else numRespawns = 0;
-        //Debug.Log(timer);
-        if (numRespawns > 0 && canStartTimer)
-        {
-            //Debug.Log("Timer Ran Out, causing respawn");
-            StartCoroutine("StartTimer");
-        }
-    }
-
-    IEnumerator StartTimer()
-    {
-        int currentLife = numRespawns;
-        canSpawn = true;
-        canStartTimer = false;
-        yield return new WaitForSeconds(waitTime);
-        if (currentLife == numRespawns) SpawnStatue();
-        
-    }
 
     public void SpawnStatue()
     {
@@ -52,14 +21,7 @@ public class TimeManager : MonoBehaviour
             canStartTimer = true;
             numRespawns--;
             canSpawn = false;
-            timer = waitTime;
-            //Debug.Log("Statue Spawned");
         }
-    }
-
-    public float GetTimer()
-    {
-        return timer;
     }
 
     public float GetLives()
@@ -71,10 +33,6 @@ public class TimeManager : MonoBehaviour
     {
         if (numRespawns >= 1) {
             SpawnStatue();
-        }
-        if (numRespawns > 0)
-        {
-            timer = waitTime;
         }
     }
 }
